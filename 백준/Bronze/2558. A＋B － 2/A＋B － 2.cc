2986cb5673ba0485df83676e13bdc61e1aaa3aed
irangi <93562388+hanlee99@users.noreply.github.com>
2025-12-31 21:24:05 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    
+    // cin은 줄바꿈이 있어도 차례대로 값을 읽어옵니다.
+    cin >> A >> B;
+    
+    cout << A + B << endl;
+    
+    return 0;
+}
