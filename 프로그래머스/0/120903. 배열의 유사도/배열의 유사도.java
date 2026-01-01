f6693411ce9641db9fc2c2c999799fdad1700208
irangi <93562388+hanlee99@users.noreply.github.com>
2026-01-01 15:34:16 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for(String s : s1)
+            for(String p : s2)
+                if(s.equals(p)) answer++;
+        return answer;
+    }
+}
