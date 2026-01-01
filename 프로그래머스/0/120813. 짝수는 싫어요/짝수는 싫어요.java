60843d8c5d9706d91d6424699b65ccf2547ee08d
irangi <93562388+hanlee99@users.noreply.github.com>
2026-01-01 15:43:31 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.07 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=1; i<=n; i++)
+            if(i%2 == 1) list.add(i);
+        answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) answer[i] = list.get(i);
+        return answer;
+    }
+}
