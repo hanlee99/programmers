5e6733d3bb8941034157dcca771c0c6156f0db05
irangi <93562388+hanlee99@users.noreply.github.com>
2026-01-01 16:06:38 +0900

    [level 0] Title: 수 조작하기 2, Time: 6.34 ms, Memory: 98.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<numLog.length-1; i++){
+            switch(numLog[i+1] - numLog[i]){
+                case 1: sb.append('w'); break;
+                case -1: sb.append('s'); break;
+                case 10:  sb.append('d'); break;             
+                case -10: sb.append('a'); break;
+            }
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
