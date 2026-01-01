784747865783cc13093a118454c070f8eaaa8382
irangi <93562388+hanlee99@users.noreply.github.com>
2026-01-01 16:50:06 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.10 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(
+            s.replace("zero","0").replace("one","1")
+            .replace("two","2").replace("three","3")
+            .replace("four","4").replace("five","5")
+            .replace("six","6").replace("seven","7")
+            .replace("eight","8").replace("nine","9").toString());
+        return answer;
+    }
+}
