9bb537dd0b7b1058adcab2f14ad28fb064af472e
irangi <93562388+hanlee99@users.noreply.github.com>
2026-01-01 16:22:34 +0900

    [level 0] Title: k의 개수, Time: 5.15 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        for(int n=i; n<=j; n++){
+            int m=n;
+            while(m>0){
+                if(m%10 == k) answer++;
+                m/=10;
+            }
+        }
+        return answer;
+    }
+}
