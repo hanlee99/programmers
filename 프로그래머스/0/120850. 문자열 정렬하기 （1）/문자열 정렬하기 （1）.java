1cca26bdd375e970c90e2dc13f09dcd4fe26c74c
irangi <93562388+hanlee99@users.noreply.github.com>
2026-01-01 15:45:38 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.48 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = {};
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<my_string.length(); i++){
+            char c = my_string.charAt(i);
+            if(c >= '0' && c<='9') list.add(c-'0');
+        }
+        
+        answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) answer[i] = list.get(i);
+        Arrays.sort(answer);
+        return answer;
+    }
+}
