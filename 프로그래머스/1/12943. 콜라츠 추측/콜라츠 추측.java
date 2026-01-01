dc0af40276a7cee29bfe2d872318df85e74e2e82
irangi <93562388+hanlee99@users.noreply.github.com>
2026-01-01 16:21:56 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n = num;
+        while(n != 1 && answer<500){
+            n = n%2==0 ? n/2 : n*3+1;
+            answer++;
+        }
+        if(answer == 500) answer = -1;
+        return answer;
+    }
+}
